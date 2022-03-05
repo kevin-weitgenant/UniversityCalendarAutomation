5f4046654a6b44268510e686e2d28f4dd092fed0
kevin-weitgenant <kevin.weitgenant@gmail.com>
2022-03-05 20:24:30 -0300

    Update Create_embeddedGoogleCalendar.py

--- Create_embeddedGoogleCalendar.py
+++ Create_embeddedGoogleCalendar.py
@@ -11,7 +11,7 @@ from Google import Create_Service, convert_to_RFC_datetime
 from dateutil.rrule import rrule, WEEKLY, MO
 from datetime import date, datetime, timedelta
 from create_calendar import ajustar
-from service import info          # COMENTAR QUANDO DER DEPLOY
+#from service import info          # COMENTAR QUANDO DER DEPLOY
 import os
 import ast
 import pytz
@@ -20,7 +20,7 @@ def init_service():
   SCOPES = ['https://www.googleapis.com/auth/calendar']
   
   # string of dictionary was passed as environment variable, this next line converts this string to a dictionary 
-  #info = ast.literal_eval(os.environ["info"])   
+  info = ast.literal_eval(os.environ["info"])   
   credentials = service_account.Credentials.from_service_account_info(
           info, scopes=SCOPES)
 
