994f3d85f2bf0507d16de2f3c864b4452a579b18
kevin-weitgenant <kevin.weitgenant@gmail.com>
2022-03-04 22:31:17 -0300

    multiple submit buttons

--- app.py
+++ app.py
@@ -16,37 +16,37 @@ app.config[ 'SECRET_KEY'] = os.environ["key"]
 def home():
     form = Horarios()
 
-    if form.validate_on_submit():
-        
-        dicionario = criar_dicionario(form.body.data)
-        form.body.data = dicionario
-        file_path = create_calendar(dicionario)
-        email = form.email.data
-        
-        if email == '':
-            return send_file(file_path, as_attachment=True)     
-        calendar_id = generate_calendar(dicionario,email)
-        
-        if calendar_id is False:
-            print("API chegou ao limite")
-            return send_file(file_path, as_attachment=True)    
+    if request.method == 'POST':
+        if request.form['submit'] == 'Calendário GoogleCalendar':
+            
+            dicionario = criar_dicionario(form.body.data)
+            form.body.data = dicionario
+            email = form.email.data
+            
+            if email == '':
+                print("faltou o e-mail")
+            
+            if calendar_id is False:
+                print("API chegou ao limite")  
 
-        else:
-            calendar_id = calendar_id.split('@')[0]
-            return redirect(url_for('getCalendar', calendar_id = calendar_id, file_path = file_path))
-    return render_template('home.html', form = form)
+            else:
+                calendar_id = calendar_id.split('@')[0]
+                return redirect(url_for('getCalendar', calendar_id = calendar_id))
+        
+        elif request.form['submit2'] == 'Arquivo .ical':
+            dicionario = criar_dicionario(form.body.data)
+            form.body.data = dicionario
+            file_path = create_calendar(dicionario)           
+            return send_file(file_path, as_attachment=True)
+
+    elif request.method == 'GET':
+        return render_template('home.html', form = form)
 
     
 @app.route('/calendario',methods=['GET','POST'] )
 def getCalendar():
     calendar_id = request.args['calendar_id']
-    file_path = request.args['file_path']
-    form = Calendario()
-
-    if form.validate_on_submit():
-        return send_file(file_path, as_attachment=True)
-    return render_template("calendario.html", calendar_id = calendar_id, file_path = file_path, form = form )    
-
+    return render_template("calendario.html", calendar_id = calendar_id)    
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
