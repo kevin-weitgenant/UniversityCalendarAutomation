cddb027157633c87f22a26daad5c6ae24d2f6ec3
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-11-06 20:30:17 -0300

    fix typo do gabriel

--- front/UniversityCalendarAutomation/src/EmbeddedGoogleCalendar.jsx
+++ front/UniversityCalendarAutomation/src/EmbeddedGoogleCalendar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import loading from './assets/loading.gif';
 
 const EmbeddedGoogleCalendar = ({ calendarId = '' }) => {
-  const calendarUrl = `https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23B39DDB&ctz=America%2/Sao_Paulo&mode=WEEK&hl=pt_BR&showCalendars=0&showPrint=0&src=${calendarId}&color=%23AD1457`;
+  const calendarUrl = `https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23B39DDB&ctz=America/Sao_Paulo&mode=WEEK&hl=pt_BR&showCalendars=0&showPrint=0&src=${calendarId}&color=%23AD1457`;
 
   const [iframeLoaded, setIframeLoaded] = useState(false);
 
