d235ee52bc7d8c5ad6de3c2175075996d5d5865a
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-11-08 17:22:05 -0300

    aviso caso nao apareça o calendario

--- front/UniversityCalendarAutomation/src/CalendarRoute.jsx
+++ front/UniversityCalendarAutomation/src/CalendarRoute.jsx
@@ -21,7 +21,9 @@ const CalendarRoute = () => {
             <div className = { styles2.calendarContainer }>
               <EmbeddedGoogleCalendar className = { styles2.calendar } calendarId={calendarId}/>
             </div>
-
+            <p style={{color: 'darkslategray', fontSize: '18px', fontStyle: 'italic', textAlign: 'center'}}>
+    Caso não consiga ver sua agenda acima, cheque no seu e-mail:
+</p>
             <div className = { styles2.imgContainer }>
                 <img src={"/email.png" } alt="Your GIF" />   
             </div>
